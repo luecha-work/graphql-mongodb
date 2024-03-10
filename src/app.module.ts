92af9e3c1280330022219d2b4fe752b6a22fb687
luecha-work <luecha.kanm@gmail.com>
2024-03-10 13:05:55 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './modules/lesson/lesson.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
